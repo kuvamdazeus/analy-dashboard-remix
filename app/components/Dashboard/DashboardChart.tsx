4dc0d18457ca766a8aae989d6e1a550d20bec55c
Kuvam Bhardwaj <kuvamyash@gmail.com>
2023-02-06 17:03:21 +0530

    - chart data is now duration-based

--- app/components/Dashboard/DashboardChart.tsx
+++ app/components/Dashboard/DashboardChart.tsx
@@ -1,12 +1,18 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Select } from "@chakra-ui/react";
 import HighchartsReact from "highcharts-react-official";
 import Highcharts from "highcharts";
 import { useMemo } from "react";
 import { loader } from "~/routes/dashboard/$pid";
-import { useLoaderData } from "@remix-run/react";
+import { useFetcher, useLoaderData } from "@remix-run/react";
+import type { getChartData } from "~/helpers/data.server";
+import { DURATIONS } from "~/constants";
 
 export default function DashboardChart() {
+  const chart = useFetcher<typeof getChartData>();
+
   const { chartData } = useLoaderData<typeof loader>();
+
+  const data = chart.data || chartData;
 
   const options: Highcharts.Options = useMemo(
     () => ({
@@ -15,7 +21,7 @@ export default function DashboardChart() {
       },
       series: [
         {
-          data: chartData.map((item) => item._count._all),
+          data: data.map((item) => item._count._all),
           type: "line",
         },
       ],
@@ -24,7 +30,7 @@ export default function DashboardChart() {
         title: {
           text: "Days",
         },
-        categories: chartData.map((item) => item.date),
+        categories: data.map((item) => item.date),
       },
       yAxis: {
         title: {
@@ -32,11 +38,35 @@ export default function DashboardChart() {
         },
       },
     }),
-    [chartData]
+    [chartData, chart.data]
   );
+
+  const fetchChartData = async (e: React.ChangeEvent<HTMLSelectElement>) => {
+    chart.load(`${window.location.pathname}/data?type=chart&duration=${e.target.value}`);
+  };
 
   return (
     <Box border="1px" borderColor="gray.100" className="bg-white rounded-lg p-1">
+      <Box className="p-5 w-full flex justify-end">
+        <Select
+          defaultValue="7d"
+          size="sm"
+          w="max"
+          rounded="lg"
+          borderColor="gray.500"
+          onChange={fetchChartData}
+        >
+          {DURATIONS.map(
+            ({ label, value }) =>
+              value !== "today" && (
+                <option key={value} value={value}>
+                  {label}
+                </option>
+              )
+          )}
+        </Select>
+      </Box>
+
       <Box flexGrow={1} borderTopColor="white" className="bg-white h-1/2">
         <HighchartsReact highcharts={Highcharts} options={options} />
       </Box>
